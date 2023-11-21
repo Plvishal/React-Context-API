803241d923932c4434503586efe3d890630f075d
Vishal <vishalpal1501@gmail.com>
2023-11-21 21:31:53 +0530

    Provide the context to parent App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Items from './components/Items';
+import { useState } from 'react';
+import { itemContext } from './itemContext';
 
 function App() {
+  const [total, setTotal] = useState(0);
+  const [item, setItem] = useState(0);
   return (
-    <div className="App">
-      <h2>Shopping Cart</h2>
-      <Navbar />
-      <Items />
-    </div>
+    <itemContext.Provider value={{ total, setTotal }}>
+      <div className="App">
+        <h2>Shopping Cart</h2>
+        <Navbar />
+        <Items />
+      </div>
+    </itemContext.Provider>
   );
 }
 
--- src/itemContext.js
+++ src/itemContext.js
@@ -1,3 +1,3 @@
 import { createContext } from 'react';
 
-export const itemCOntext = createContext();
+export const itemContext = createContext();
