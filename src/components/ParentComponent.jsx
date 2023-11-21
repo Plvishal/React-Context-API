ebefbbb542b5daa5343288cd83589e5f08b57921
Vishal <vishalpal1501@gmail.com>
2023-11-21 17:26:26 +0530

    provide the context

--- src/components/ParentComponent.jsx
+++ src/components/ParentComponent.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ChildComponen from './ChildComponen';
+import { colorContext } from '../context';
 
-function ParentComponent() {
-  return <div>ParentComponent</div>;
+function ParentComponent(props) {
+  const [color, setColor] = useState('#000000');
+  return (
+    <>
+      <h1>Pick a color</h1>
+      <input
+        type="color"
+        onChange={(e) => {
+          setColor(e.target.value);
+        }}
+        value={color}
+      />
+      <colorContext.Provider color="red">
+        <ChildComponen />
+      </colorContext.Provider>
+    </>
+  );
 }
 
 export default ParentComponent;
