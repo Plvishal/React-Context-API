dc3d32c0b87115182e560113c587a633ab1e6c90
Vishal <vishalpal1501@gmail.com>
2023-11-21 21:37:06 +0530

    Consume the value where you want in this condition we consume the value inside the Navbar.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-import styles from "../styles/Total.module.css";
+import React from 'react';
+import styles from '../styles/Total.module.css';
+import { useContext } from 'react';
+import { itemContext } from '../itemContext';
 
 function Navbar() {
+  const value = useContext(itemContext);
+  console.log(value);
   return (
     <div className={styles.container}>
       <h1>Total : &#x20B9; 0</h1>
